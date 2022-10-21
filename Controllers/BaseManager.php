fdacf71a365a64ef6ff64a5dc5593f94e27684b1
Earnox <87123482+Earnox@users.noreply.github.com>
2022-10-21 17:39:55 +0200

    try to connect

--- Controllers/BaseManager.php
+++ Controllers/BaseManager.php
@@ -8,6 +8,17 @@ class BaseManager
 
     $dbName = "tech_vannilla";
     $this->setPdo(new PDO("mysql:host=localhost:3307;dbname=$dbName", 'root', ''));
+
+
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"], 1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $this->pdo = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
   }
 
   /**
--- Controllers/CommentsManager.php
+++ Controllers/CommentsManager.php
@@ -1,9 +1,19 @@
 <?php
+
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 class CommentsManager extends BaseManager
 {
   public function create(Comment $comment)
   {
-    $req = $this->pdo->prepare("INSERT INTO `commentaire`(content,article_id) VALUES (:content, :article_id)");
+    $req = $this->conn->prepare("INSERT INTO `commentaire`(content,article_id) VALUES (:content, :article_id)");
     $req->bindValue(":content", $comment->getContent(), PDO::PARAM_STR);
     $req->bindValue(":article_id", $comment->getArticle_id(), PDO::PARAM_INT);
     $req->execute();

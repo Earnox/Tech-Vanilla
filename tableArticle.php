fd2656172a54ae80dd4f18401efba3c18beec545
Earnox <87123482+Earnox@users.noreply.github.com>
2022-10-29 12:00:27 +0200

    change for pdo dev and production

--- tableArticle.php
+++ tableArticle.php
@@ -12,16 +12,7 @@
       $articles = $manager->getall();
       foreach ($articles as $article) : ?>
 
-        <div class=" m-2 card" style="width: 18rem;">
-          <!-- <img src="..." class="card-img-top" alt="..."> -->
-          <div class="card-body">
-            <h5 class="card-title"><?= $article->getTitle() ?></h5>
-            <p class="card-text"><?= substr($article->getContent(), 0, 30) ?> </p>
-            <a href="./readArticle.php?id=<?= $article->getId() ?>" class=""><i class="fas fa-eye"></i> Lire plus...</a>
-            <a href="./uptateArticle.php?id=<?= $article->getId() ?>" class="btn btn-warning"><i class="fas fa-edit"></i></a>
-            <a href="./deleteArticle.php?id=<?= $article->getId() ?>" class=" btn btn-danger"><i class="fas fa-trash-alt"></i></a>
-          </div>
-        </div>
+
 
         <tr>
           <th scope="row"><?= $article->getTitle() ?></th>
